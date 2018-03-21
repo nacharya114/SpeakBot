67b639f65a93b4f622b4450c17d23e59e9668f0a
nacharya114 <nacharya114@gmail.com>
2018-03-21 16:57:31 +0100

    added firebase environmental variables

--- server/auth.js
+++ server/auth.js
@@ -3,11 +3,11 @@ require('firebase/auth');
 require('firebase/database');
 // Initialize Firebase for the application
 var config = {
-    apiKey: process.env.apiKey,
-    authDomain: process.env.authDomain,
+    // apiKey: process.env.apiKey,
+    // authDomain: process.env.authDomain,
     databaseURL: process.env.databaseURL,
-    storageBucket: process.env.storageBucket,
-    messagingSenderId: process.env.messagingSenderId
+    // storageBucket: process.env.storageBucket,
+    // messagingSenderId: process.env.messagingSenderId
   };
 
 firebase.initializeApp(config);
